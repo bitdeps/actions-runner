f052f1a39db2d3e668f6b3498afce6e24a892bf9
Denis Baryshev <dennybaa@gmail.com>
2024-03-14 22:05:47 +0700

    fix image pull cache
    Signed-off-by: Denis Baryshev <dennybaa@gmail.com>

--- runners/entrypoint.sh
+++ runners/entrypoint.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
-# update runner
-install-runner
+## Preload images
+pull_images() {
+  local pull_tags; declare -A pull_tags
+  pull_tags=(
+    ["images/ubuntu"]="latest 22.04"
+  )
 
-# prepull containers
-grep 'bitdeps/actions-runner/images/' < /etc/containers/registries.conf.d/000-shortnames.conf | sed -e 's@=.*@@' -e 's@"@@g' | xargs -n1 podman pull
+  echo "=> Updating local image cache..."
+  grep 'bitdeps/actions-runner/images/' < /etc/containers/registries.conf.d/000-shortnames.conf | sed -e 's@=.*@@' -e 's@"@@g' | while read -r alias; do
+    if [[ -n "${pull_tags[$alias]}" ]]; then
+      for tag in ${pull_tags[$alias]}; do
+        podman pull "${alias}:${tag}"
+      done
+    fi
+  done
+}
+
+# Update runner
+install-runner
+pull_images
 
 podman system service --time=0 &
 exec bash
