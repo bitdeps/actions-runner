d11d9c29c1cd6be96664dd7360ef49373f2ca6fe
Denis Baryshev <dennybaa@gmail.com>
2024-03-14 21:41:55 +0700

    add aliases for container jobs
    Signed-off-by: Denis Baryshev <dennybaa@gmail.com>

--- runners/entrypoint.sh
+++ runners/entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+
 # update runner
 install-runner
+
+# prepull containers
+grep 'bitdeps/actions-runner/images/' < /etc/containers/registries.conf.d/000-shortnames.conf | sed -e 's@=.*@@' -e 's@"@@g' | xargs -n1 podman pull
+
 podman system service --time=0 &
 exec bash
