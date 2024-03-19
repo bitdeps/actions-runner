7c5c8ab879fbd86cd2879ef87443a94e3bd92fee
Denis Baryshev <dennybaa@gmail.com>
2024-03-20 04:32:07 +0700

    link podman and docker to podman-remote.sh
    Signed-off-by: Denis Baryshev <dennybaa@gmail.com>

--- runners/podman-remote.sh
+++ runners/podman-remote.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export CONTAINER_HOST=unix:///runner/podman.sock
 export DOCKER_HOST=unix:///runner/podman.sock
-exec /runner/tools/bin/podman --remote "$@"
+exec /runner/tools/bin/podman-static --remote "$@"
