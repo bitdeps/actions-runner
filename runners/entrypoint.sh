2acbca7298ad8ef1788ce52e859511075bb7f506
Denis Baryshev <dennybaa@gmail.com>
2024-12-28 13:27:39 +0700

    feat: migrate to bitdeps/podman-static 5.x version
    Signed-off-by: Denis Baryshev <dennybaa@gmail.com>

--- runners/entrypoint.sh
+++ runners/entrypoint.sh
@@ -3,9 +3,6 @@
 # Start podman socket
 podman system service --time=0 &
 
-# Update and use the recent runner, it's preferrable for better API compatibility.
-install-runner
-
 if (! which "$1" 1>/dev/null 2>/dev/null ) && [ ! -x "$1" ]; then
   # expect script or bash commands
   set -- bash "$@"
